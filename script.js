cc47184966855b8db1a30d20a0b8c11d4f7ffbe3
alok-404 <alokchandra2621@gmail.com>
2026-01-21 21:50:05 +0530

    Initial DOM todo app with localStorage

---
+++ script.js
@@ -0,0 +1,59 @@
+let input = document.querySelector(".input-box #todoInput");
+let btn = document.querySelector("#addBtn");
+let todoList = document.querySelector("#todoList");
+let ul = document.querySelector(".app #todoList");
+
+ let allTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+btn.addEventListener("click",function(){
+  let newTodo = input.value.trim();
+  if(newTodo === "") return;
+
+  let todoObj = {
+    text: newTodo,
+    completed: false
+  }
+  todoObj.push(allTodos)
+
+  input.value = ""
+})
+allTodos.forEach(todoObj => {
+  createTodoUI(todoObj);
+});
+
+function createTodoUI(todoObj){
+  let list = document.createElement("li");
+
+  if (todoObj.completed) {
+    list.classList.add("done");
+  }
+
+  let p = document.createElement("p");
+  p.textContent = todoObj.text;
+  list.appendChild(p);
+
+  let dltbtn = document.createElement("span");
+  dltbtn.textContent = "🗑️";
+  dltbtn.classList.add("delete-btn");
+  list.appendChild(dltbtn);
+
+  ul.appendChild(list);
+
+// delete task
+  dltbtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+
+    allTodos = allTodos.filter(t => t !== todoObj);
+    localStorage.setItem("todos", JSON.stringify(allTodos));
+
+    list.remove();
+  });
+
+  // toggle complete
+  list.addEventListener("click", function () {
+    todoObj.completed = !todoObj.completed;
+    list.classList.toggle("done");
+
+    localStorage.setItem("todos", JSON.stringify(allTodos));
+  });
+}
